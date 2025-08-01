7e8374f7dd021d8564abed0558c5997688abc2f6
chen.xiang <chen.xiang@appshahe.com>
2025-08-01 11:17:29 +0800

    Model

---
+++ Source/ModelGen/ModelGen.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ModelGen.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ModelGen, "ModelGen" );
